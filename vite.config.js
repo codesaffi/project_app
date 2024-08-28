18998930507488de12b456aff23c8ed9c398626c
codesaffi <saffi8946@gmail.com>
2024-08-29 03:46:41 +0500

    f

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
    base: "/project_app/",
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000',
-    },
-  }
+  // server: {
+  //   proxy: {
+  //     '/api': 'http://localhost:5000',
+  //   },
+  // }
 })
