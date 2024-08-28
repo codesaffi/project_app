b5b976b86785a549b0a782af3d752011ff47c6d0
codesaffi <saffi8946@gmail.com>
2024-08-29 03:28:19 +0500

    feat: routing

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: "/project_app/",
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
     },
-  },
-  base: "/project_app/"
+  }
 })
